4d078c027e610a62e4f36affd12a27e2d3628c2d
Anish George <55911569+ANISHG-26@users.noreply.github.com>
2024-02-25 16:13:54 -0500

    Added new test urls

--- test_api.py
+++ test_api.py
@@ -2,7 +2,9 @@ import requests
 
 # Endpoint URL
 local_url = 'http://localhost:5000/predict'  # Update the URL if necessary
-heroku_url = "https://dementia-prediction-a1a3078b20c1.herokuapp.com/upload"
+heroku_docker_url = "https://dementia-prediction-a1a3078b20c1.herokuapp.com/upload"
+heroku_url = "https://dementia-prediction-2-207aa71652df.herokuapp.com/upload"
+ehospital_heroku_url = "https://dementia-api2024-3db4f4251edb.herokuapp.com/upload"
 
 # Image file path
 image_file = 'images/26.jpg'  # Replace with the path to your image file
@@ -13,7 +15,7 @@ with open(image_file, 'rb') as f:
     files = {'image': f}
 
     # Send the POST request to the API
-    response = requests.post(heroku_url, files=files)
+    response = requests.post(ehospital_heroku_url, files=files)
 
     # Check if the request was successful (status code 200)
     if response.status_code == 200:
